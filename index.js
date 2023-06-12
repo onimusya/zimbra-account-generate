62a1b32501c7f2fa13a8533fea531e2db0a39b35
Francis Hor <francishoe@gmail.com>
2023-06-12 15:44:41 +0800

    Update

---
+++ index.js
@@ -0,0 +1,62 @@
+const { generateFromEmail, generateUsername } = require("unique-username-generator");
+const path = require('path');
+const fs = require('fs');
+const { exec } = require("child_process");
+
+
+let generateAccounts = async function (count, emailHost, password) {
+    for (let i=0; i<count; i++) {
+        let username = generateUsername();
+        let email = username + '@' + emailHost;
+    
+        console.log(`Generate ${i+1} -> ${email}    password:${password}`);
+        let command = `zmprov createAccount ${email} ${password} displayName '${username}' givenName ${username} sn '.'`;
+        console.log(`Command: ${command}`);
+        
+    }
+    
+}
+
+// Retrieve the command line arguments
+let argv = require('minimist')(process.argv.slice(2), { string: [] });
+
+// Parameters
+// -n   : Total accounts to generate
+// -h   : Email host name eg: mememail.xyz
+// -p   : Default password
+
+let missingParams = false;
+
+if (typeof argv.n == "undefined") {
+    missingParams = true;
+}
+
+if (typeof argv.h == "undefined") {
+    missingParams = true;
+}
+
+if (typeof argv.p == "undefined") {
+    missingParams = true;
+}
+
+if (missingParams) {
+
+    console.log(`
+Missing parameters!
+
+Parameters
+    -n  : Total accounts to generate
+    -h  : Email host name
+    -p  : Default password
+    `);
+
+    return;
+}
+
+if (argv.n <= 0) {
+    console.log(`Parameter "n" must has value larger than 0.`);
+    return;
+}
+
+generateAccounts(argv.n, argv.h, argv.p);
+
